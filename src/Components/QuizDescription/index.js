44a2bb8744c80d83dcaeff9b8cab3b65958c4af6
Mohamed <mohamed@host-156.192.146.129-static.tedata.net>
2019-09-13 21:42:20 +0200

    init: quiz description component

---
+++ src/Components/QuizDescription/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+const QuizDescription = () => {
+  return (
+    <div className="QuizDescriptionContainer">
+      <h1 className="Title">Shit</h1>
+      <p className="Description">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's
+      </p>
+    </div>
+  );
+};
+
+export default QuizDescription;
