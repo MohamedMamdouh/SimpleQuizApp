3dcb984f5bc0e9200adb0b64f87f7f3ef02e7d14
Mohamed <mohamed@host-156.192.146.129-static.tedata.net>
2019-09-13 21:42:41 +0200

    init: result component

---
+++ src/Components/Result/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+
+const Result = () => {
+  return (
+    <div className="ResultContainer">
+      <h1 className="ResultTitleText">Result</h1>
+      <span className="ResultText">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's
+      </span>
+      <span className="TryAgainhref">Try Again</span>
+    </div>
+  );
+};
+
+export default Result;
