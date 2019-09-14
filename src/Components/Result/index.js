0852b195d1271454d7a843f8ab378b081b47e9cc
Mohamed <mohamed@Macbooks-MacBook-Pro.local>
2019-09-14 02:22:41 +0200

    feat: create result component

--- src/Components/Result/index.js
+++ src/Components/Result/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
-import './style.css';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
-const Result = () => {
+// Styled component for Result Container
+const ResultContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding-left: 1rem;
+  width: 100%;
+`;
+
+// Styled component for Result text
+const ResultText = styled.span`
+  text-align: start;
+  margin-bottom: 1rem;
+`;
+
+// Styled Component for Try again button
+const TryAgainButton = styled.span`
+  text-decoration: underline;
+  margin-bottom: 1rem;
+  cursor: pointer;
+`;
+
+const Result = ({ onResetResult, results }) => {
+  const yesNumbers = results.filter(item => item === 'yes').length;
   return (
-    <div className="ResultContainer">
-      <h1 className="ResultTitleText">Result</h1>
-      <span className="ResultText">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's
-      </span>
-      <span className="TryAgainhref">Try Again</span>
-    </div>
+    <ResultContainer className="ResultContainer">
+      <h1>Result</h1>
+      <ResultText className="ResultText">
+        {yesNumbers !== 0
+          ? `You answered 'yes' ${yesNumbers} times. That indicates you could have
+        some use of our product. We Recommed trying our free trial.`
+          : `You didn't answere any 'yes'. That indicates you could try other product.`}
+      </ResultText>
+      <TryAgainButton className="TryAgainhref" onClick={onResetResult}>
+        Try Again
+      </TryAgainButton>
+    </ResultContainer>
   );
+};
+
+Result.propTypes = {
+  onResetResult: PropTypes.func.isRequired,
+  results: PropTypes.array.isRequired
 };
 
 export default Result;
